7492f5719e3da7437af142897e04667c51c47f93
Robert L. Svarinskis <rlsvarinskis@gmail.com>
2019-09-30 16:25:06 +0300

    Line

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -23,4 +23,8 @@ graphics.beginFill(0xDE3249);
 graphics.drawRect(50, 50, 100, 100);
 graphics.endFill();
 
+graphics.lineStyle(69, 0x1337FF, 1, 0.5);
+graphics.moveTo(69, 420);
+graphics.lineTo(420, 420);
+
 app.stage.addChild(graphics);
