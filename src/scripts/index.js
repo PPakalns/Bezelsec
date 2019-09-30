5c5d3fe49a6c9f767fbf82aa7674412b7180a87a
Robert L. Svarinskis <rlsvarinskis@gmail.com>
2019-09-30 16:16:49 +0300

    Fullscreen canvas

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -11,7 +11,7 @@ if(!PIXI.utils.isWebGLSupported()){
 PIXI.utils.sayHello(type);
 
 //Create a Pixi Application
-let app = new PIXI.Application({width: 256, height: 256});
+let app = new PIXI.Application({width: window.innerWidth, height: window.innerHeight});
 
 //Add the canvas that Pixi automatically created for you to the HTML document
 document.body.appendChild(app.view);
