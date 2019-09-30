58d0c30109e59c7365012f16097fcf79d3a09bd8
peteris.pakalns <peteris.pakalns@kpiberry.com>
2019-09-30 16:09:04 +0300

    Beginning changes

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,26 @@
 import '../styles/index.scss';
+import * as PIXI from 'pixi.js';
 
-console.log('webpack starterkit');
+console.log("TEST");
+
+let type = "WebGL";
+if(!PIXI.utils.isWebGLSupported()){
+    type = "canvas";
+}
+
+PIXI.utils.sayHello(type);
+
+//Create a Pixi Application
+let app = new PIXI.Application({width: 256, height: 256});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+const graphics = new PIXI.Graphics();
+
+// Rectangle
+graphics.beginFill(0xDE3249);
+graphics.drawRect(50, 50, 100, 100);
+graphics.endFill();
+
+app.stage.addChild(graphics);
